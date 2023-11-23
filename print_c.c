7b192550e835ff822ae4768cc4f06f8b08dedfe5
HB74C21 <6720@holbertonstudents.com>
2023-11-23 00:48:04 -0800

    Add program print_c.c

---
+++ print_c.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_c - Function that print a type char
+ *
+ * @ap: This is a list of argument pointed to a type char
+ *
+ * Return: a integer
+*/
+int print_c(va_list ap)
+{
+	char letter;
+
+	letter = va_arg(ap, int);
+
+	write(1, &c, 1);
+
+	return (1);
+}
